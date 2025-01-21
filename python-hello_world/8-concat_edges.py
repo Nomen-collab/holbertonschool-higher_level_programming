5cd24fc99c0e7328ec03b5627d822472d985ec81
Nomen-collab <nomenaratsimba01@gmail.com>
2025-01-21 20:59:35 +0000

    exo7 python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Extraire et réarranger les parties nécessaires
 str = str[39:67] + str[107:112]
 print(str)
