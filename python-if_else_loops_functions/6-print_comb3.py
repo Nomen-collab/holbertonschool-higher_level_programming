17447afc13ce893a1ef38f6f797aab7f762b6338
Nomen-collab <nomenaratsimba01@gmail.com>
2025-01-21 22:03:37 +0000

    exo6 python

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j), end="" if i < 8 else "\n")
+        if i == 8 and j == 9:
+            print("{:02d}".format(i * 10 + j))
+        else:
+            print("{:02d}, ".format(i * 10 + j), end="")
