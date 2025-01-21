8c06c63955f20a4661d53f0fa1df2858163f7eda
Nomen-collab <nomenaratsimba01@gmail.com>
2025-01-21 23:07:09 +0000

    exo6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()i
+def print_matrix_integer(matrix=[[]]):
+    lenght = len(matrix)
+    for x in range(lenght):
+        for y in range(len(matrix[x])):
+            print('{:d}'.format(matrix[x][y]), end='')
+            if y is not (len(matrix[x]) - 1):
+                print(end=" ")
+        print("")
