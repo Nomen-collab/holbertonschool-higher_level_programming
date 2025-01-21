b10f14b9047f51288b4922c6448e36b00c5bff28
Nomen-collab <nomenaratsimba01@gmail.com>
2025-01-21 22:12:48 +0000

    exo8 python

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    result = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end="")
+            result += chr(ord(char) - 32)
         else:
-            print(char, end="")
-    print()
+            result += char
+    print("{}".format(result))
