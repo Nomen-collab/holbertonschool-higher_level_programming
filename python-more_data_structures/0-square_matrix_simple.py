e35931a62f7ac60182a244b42bc4ba564a293d89
Nomen-collab <nomenaratsimba01@gmail.com>
2025-01-22 10:37:43 +0000

    exo0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,11 @@
 def square_matrix_simple(matrix=[]):
-    """
-    Compute the square value of all integers in a matrix.
-    
-    Args:
-        matrix (list of lists): 2D array of integers.
-        
-    Returns:
-        list of lists: New matrix with squared values, same size as input matrix.
-    """
-    # Utiliser une compréhension de liste pour créer une nouvelle matrice avec les carrés des valeurs
-    return [[element ** 2 for element in row] for row in matrix]
+	"""
+	Compute the square of all integers in a matrix.
+
+	Args:
+		matrix (list of lists): A 2D list of integers.
+
+	Returns:
+		list of lists: A new matrix with each integer squared.
+	"""
+	return [[value ** 2 for value in row] for row in matrix]
