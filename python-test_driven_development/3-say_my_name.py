7c3722311488e890b798888ee472addfc6fe3e9a
Nomen-collab <nomenaratsimba01@gmail.com>
2025-05-16 16:02:04 +0200

    fix exo2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
+"""
+Function that prints My name is <first name> <last name>
+
+
+"""
+
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints "My name is <first_name> <last_name>".
-    Parameters:
-    first_name (str): The first name of the person.
-    last_name (str, optional): The last name of the person.\
-          Defaults to an empty string.
-    Raises:
-    TypeError: If either `first_name` or `last_name` is not a string.
-    Example:
-    >>> say_my_name("John", "Smith")
-    My name is John Smith
-    >>> say_my_name("Walter")
-    My name is Walter
-    >>> say_my_name(12, "White")
-    Traceback (most recent call last):
-        ...
-    TypeError: first_name must be a string
+    Prints a Name
+
+    Arguments:
+
+    first_name: must be a string
+
+    last_name: must be a string, if no argument given, then empty by default
+
+    Otherwise a TypeError is raised
+
     """
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
+        msg = "first_name must be a string"
+        raise TypeError(msg)
 
-    print(f"My name is {first_name} {last_name}".strip())
+    if not isinstance(last_name, str):
+        msg = "last_name must be a string"
+        raise TypeError(msg)
+
+    print("My name is {} {}".format(first_name, last_name))
