4747e001f8cb5bb25e58d717e778b82bbc713dd7
Nomen-collab <nomenaratsimba01@gmail.com>
2025-06-19 14:07:16 +0200

    fix task 05

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -72,4 +72,4 @@ def handle_revoked_token_error(err):
 def handle_needs_fresh_token_error(err):
     return jsonify({"error": "Fresh token required"}), 401
 if __name__ == '__main__':
-    app.run()²
+    app.run()
