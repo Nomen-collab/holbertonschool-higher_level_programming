67839aa2198b14da19117a5d1d30d129a5d6593e
Nomen-collab <nomenaratsimba01@gmail.com>
2025-07-24 12:36:47 +0200

    Fix taks 0

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // Déclare une constante nommée myVar avec la valeur "JavaScript is amazing"
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing'; // Changement : guillemets doubles remplacés par des guillemets simples
 
 // Affiche la valeur de myVar dans la console
 console.log(myVar);
+// Le fichier se termine ici, sans lignes vides supplémentaires après cette ligne (juste un seul saut de ligne final)
 
