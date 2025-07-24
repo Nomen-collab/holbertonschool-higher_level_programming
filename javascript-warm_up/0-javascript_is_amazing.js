c50678e0298d1e579d30dc6f34c08273b0dd2981
Nomen-collab <nomenaratsimba01@gmail.com>
2025-07-24 12:29:02 +0200

    add task 1

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+// Déclare une constante nommée myVar avec la valeur "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+
+// Affiche la valeur de myVar dans la console
+console.log(myVar);
+
