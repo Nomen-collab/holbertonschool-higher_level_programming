9120dd2389100c2ba8acf62cd8453ea306cd7fb9
Nomen-collab <nomenaratsimba01@gmail.com>
2025-07-24 12:48:59 +0200

    Fix task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,16 +3,18 @@
 // process.argv est un tableau contenant les arguments de la ligne de commande.
 // Le premier élément (index 0) est le chemin de l'exécutable node.
 // Le deuxième élément (index 1) est le chemin du fichier de script exécuté.
-// Les arguments réels commencent à l'index 2.
-// Donc, le nombre d'arguments passés par l'utilisateur est la longueur totale de process.argv moins 2.
-const numberOfArguments = process.argv.length - 2;
+// Les arguments passés par l'utilisateur commencent à l'index 2.
 
-// Vérifie le nombre d'arguments et affiche le message approprié.
-if (numberOfArguments === 0) {
+// Vérifie la présence des arguments en accédant directement aux index.
+if (process.argv[2] === undefined) {
+  // Si process.argv[2] n'existe pas, cela signifie qu'aucun argument n'a été passé.
   console.log('No argument');
-} else if (numberOfArguments === 1) {
+} else if (process.argv[3] === undefined) {
+  // Si process.argv[2] existe mais process.argv[3] n'existe pas,
+  // cela signifie qu'un seul argument a été passé.
   console.log('Argument found');
 } else {
+  // Si process.argv[3] existe, cela signifie que plusieurs arguments ont été passés.
   console.log('Arguments found');
 }
 // Le fichier se termine ici avec un seul saut de ligne final.
