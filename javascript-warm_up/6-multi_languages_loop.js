ae6113d3ae332c22ff8f246ed7b56f71f17d805a
Nomen-collab <nomenaratsimba01@gmail.com>
2025-07-24 13:04:17 +0200

    fix task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// Déclare une constante nommée 'languages' qui est un tableau de chaînes.
-// Chaque chaîne représente une ligne à afficher.
 const languages = [
   'C is fun',
   'Python is cool',
   'JavaScript is amazing'
 ];
 
-console.log(languages.join('\n'));
+for (const lang of languages) {
+  console.log(lang);
+}
