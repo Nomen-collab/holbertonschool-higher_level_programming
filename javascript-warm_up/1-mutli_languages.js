1e3d2ac14e88d7b2c11eac3d9bde6a3372fd5046
Nomen-collab <nomenaratsimba01@gmail.com>
2025-07-24 12:43:38 +0200

    Task 1

---
+++ javascript-warm_up/1-mutli_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Affiche la première ligne en utilisant des guillemets simples
+console.log('C is fun');
+
+// Affiche la deuxième ligne en utilisant des guillemets simples
+console.log('Python is cool');
+
+// Affiche la troisième ligne en utilisant des guillemets simples
+console.log('JavaScript is amazing');
+// Le fichier se termine ici, juste après le point-virgule de la dernière ligne,
+// avec seulement un seul saut de ligne final (invisible).
